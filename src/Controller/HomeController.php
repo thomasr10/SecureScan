a913b7090e36f0745d30f32314fe6a2e3dabbc8b
thomasr10 <t.rivoire01@gmail.com>
2026-03-02 15:51:31 +0100

    clone repo in /projects

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Uid\Uuid;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function showHome(): Response
+    {   
+
+    // On affiche la homepage
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    
+    #[Route('/upload', name: 'app_upload', methods: ['POST'])]
+    public function upload(Request $request): Response
+    {
+        $url = $request->request->get('project_url');
+        $zip = $request->request->get('project_zip');
+
+        if ($url && !$zip) {
+            
+            // checker si url est bien un .git
+            $url = trim($url);
+            if(!str_ends_with($url, '.git')) {
+                $error = "L'URL doit être un dépôt valide";
+            }
+
+            // clone du repo
+            
+            // créer un dossier unique
+            $projectId = 'project_' . Uuid::v4();
+
+            // process = composant symfo qui permet d'exécuter des commandes
+            $projectsDir = $this->getParameter('kernel.project_dir') . '/projects' . '/' . $projectId;  
+            $process = new Process(['git', 'clone', $url, $projectsDir]);
+            $process->run();
+
+            if (!$process->isSuccessful()) {
+                throw new ProcessFailedException($process);
+            }
+
+        }
+
+        if ($zip && !$url) {
+
+        }
+
+        return $this->redirectToRoute('app_home');
+    }
+}
